9ff2e165137ad36a1746eeebf8a3e679484639c4
Will Venn <infohack3@gmail.com>
2014-11-29 16:04:06 +0000

    Added Main application

---
+++ CPU-Optimizer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3.4
+
+from tkinter import *
+#----------------------------------------
+
+root = Tk()
+root.title("CPU Optimizer")
+root.geometry("450x250")
+#----------------------------------------
+
